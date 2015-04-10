e68b56e2eb79c9e200f347668e5f13d7aafe6501
Nix3n <martin.eriksson0136@stud.hkr.se>
2015-04-10 14:18:31 +0200

    Atmel Test

---
+++ Test1/Test1/Test1.c
@@ -0,0 +1,19 @@
+/*
+ * Test1.c
+ *
+ * Created: 2014-05-27 15:41:27
+ *  Author: Martin
+ */ 
+
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+	
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
